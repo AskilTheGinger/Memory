89a0e625e1f081ec63b6d19c4cabd3539587485c
AskilTheGinger <askilab@outlook.com>
2025-01-10 19:41:41 +0100

    format

--- js/script.js
+++ js/script.js
@@ -15,17 +15,17 @@ let x = 0
 let forsøk = 14
 let image1 = ""
 let openedImage = document.getElementsByClassName("opened")
-let closedImage= document.getElementsByClassName("closed")
-function wrong(){
+let closedImage = document.getElementsByClassName("closed")
+function wrong() {
     for (let i = 0; i < openedImage.length;) {
         openedImage[i].classList.replace("opened", "closed")
         console.log(openedImage)
-        x=0
-        
-        document.getElementById("forsøk").innerHTML = "Forsøk: " +  forsøk
+        x = 0
+
+        document.getElementById("forsøk").innerHTML = "Forsøk: " + forsøk
     }
     forsøk--
-} 
+}
 //Fisher-Yates shuffle som er den mest populære korrekte måten å shuffle en array.
 function shuffle(array) {
     let currentIndex = array.length;
@@ -44,7 +44,7 @@ function shuffle(array) {
 }
 function turnImage(element) {
 
-    if (x == 1 && element.classList.contains("closed") === true){
+    if (x == 1 && element.classList.contains("closed") === true) {
 
         if (element.getAttribute("style") === image1) {
             x = 0
@@ -57,36 +57,36 @@ function turnImage(element) {
             x++
             element.classList.replace("closed", "opened")
             setTimeout(wrong, 1000)
-            setTimeout(tapte,1000)
+            setTimeout(tapte, 1000)
         }
-    } else if( x < 1 &&element.classList.contains("closed") === true) {
-        
+    } else if (x < 1 && element.classList.contains("closed") === true) {
+
         console.log(element.classList)
         element.classList.replace("closed", "opened")
-    //Henter bakgrunnsbildet som elementet har og lagrer det som en string
+        //Henter bakgrunnsbildet som elementet har og lagrer det som en string
         image1 = element.getAttribute("style")
-        
+
 
         x++
     }
-    let closedImage= document.getElementsByClassName("closed")
-    if (closedImage.length < 1 && document.getElementsByClassName("cleared").length >0){
-        document.getElementById("main").innerHTML= "<h1> BRA JOBBA!! </h1>"
+    let closedImage = document.getElementsByClassName("closed")
+    if (closedImage.length < 1 && document.getElementsByClassName("cleared").length > 0) {
+        document.getElementById("main").innerHTML = "<h1> BRA JOBBA!! </h1>"
         console.log("du klarte det wow!")
         document.getElementById("main").style.background = "none"
         document.getElementById("main").style.border = "none"
     }
-   
+
 }
-function tapte(){
-    if (forsøk==-1 ){
-        document.getElementById("main").innerHTML= "<h1> DU TAPTE D: </h1>"
+function tapte() {
+    if (forsøk == -1) {
+        document.getElementById("main").innerHTML = "<h1> DU TAPTE D: </h1>"
         console.log("du tapte")
         document.getElementById("main").style.background = "none"
-         document.getElementById("main").style.border = "none"
+        document.getElementById("main").style.border = "none"
     }
 }
-function assignImage(){
+function assignImage() {
     let doubleImage = images_array.concat(images_array)
 
     shuffle(doubleImage)
